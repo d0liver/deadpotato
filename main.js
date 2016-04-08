e523597778e6d329a9be226b6cfc36d378c340fe
David Oliver <david@doliver.org>
2016-04-08 10:32:08 -0500

    More fun

--- main.js
+++ main.js
@@ -42,34 +42,69 @@ $(document).ready(function () {
 });
 
 var darken = function (css_hex, pct_amount) {
-   /* Remove the leading # */
-   css_hex = css_hex.slice(1);
-   var result = "";
+   if (pct_amount < 0)
+      console.log("Color in: ", css_hex);
+   var colors = colorsToArray(css_hex);
 
-   for (i = 1; i <= 3; ++i) {
-      var this_num = parseInt(css_hex.slice(0, 2), 16);
-      var amount = this_num*(pct_amount/100) & ~0;
-      this_num = Math.max(0, this_num - amount);
-      var hex = this_num.toString(16);
-      result += (hex.length == 1)? "0"+hex:hex;
+   /* Darken the colors */
+   colors = _.map(colors, function (color) {
+      var amount = color*pct_amount/100 & ~0;
+      /* The Math.min is because we can accept negative values */
+      return Math.min(255, Math.max(color - amount, 0));
+   });
+
+   if (pct_amount < 0)
+      console.log("Color out: ", arrayToColors(colors));
+   return arrayToColors(colors);
+};
+
+var colorsToArray = function (css_hex) {
+   var colors = [];
+   /* Remove the leading '#' */
+   css_hex = css_hex.slice(1);
+
+   for (var i = 1; i <= 3; ++i) {
+      /* Grab this color */
+      colors.push(parseInt(css_hex.slice(0, 2), 16));
       /* Shrink the string for the next iteration */
       css_hex = css_hex.slice(2);
    }
 
+   return colors;
+};
+
+var arrayToColors = function (colors) {
+   var result = "";
+
+   for (var i = 0; i < 3; ++i) {
+      var hex = colors[i].toString(16);
+      result += (hex.length == 1)? "0"+hex:hex;
+   }
+
    return "#"+result;
+};
+
+var rgbaCssFromHex = function (css_hex, alpha) {
+   var colors = colorsToArray(css_hex);
+
+   return "rgba("+colors.join(",")+","+alpha+")";
 };
 
 var showInteractions = function (gam_info, color_map) {
    var countries = gam_info.countries();
 
    for (var i = 0; i < countries.length; ++i) {
-      var color = darken(color_map.map(countries[i].color.toLowerCase()), 50);
+      var hex_color = color_map.map(countries[i].color.toLowerCase());
+      var color = darken(hex_color, 50);
+      var gradient_dark = darken(hex_color, 70);
+      var text_color = darken(hex_color, -20);
       $(".interactions ul").append(
          "<li style='border-color: "+color+
-         "; border-bottom: solid black 1px;'>"+
-         "<span style='color: "+
-            color+
-         "' class='label'>"+
+         "; border-bottom: solid black 1px; background: linear-gradient(90deg, "+
+         rgbaCssFromHex(gradient_dark, 1)+","+
+         rgbaCssFromHex(color, 0.7)+
+         ");'>"+
+         "<span style='color: white;' class='label'>"+
             countries[i].name+
          "</span>"
          // "<img class='icon' src='"+icons[i].canvas.toDataURL()+"'/></li>"
